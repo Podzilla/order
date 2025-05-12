94a14a8e7d381129e439c2a251dcf999caf5cad3
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2025-05-12 20:46:47 +0300

    added order address

---
+++ src/main/java/com/podzilla/order/model/Address.java
@@ -0,0 +1,30 @@
+package com.podzilla.order.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+}

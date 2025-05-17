e0d05d76b30177aeaa0072414404410fca43955a
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2025-05-17 22:23:11 +0300

    Fixed run errors (#5)
    * added project base
    
    * added .github file
    
    * fixed linter issue
    
    * fixed linter issue
    
    * added order model, repository, service and controller
    
    * fixed linter issues
    
    * Add endpoints to get orders by user ID, cancel orders, and update order status
    
    * Fix linter issue
    
    * Resolve Comments
    
    1. Added Swagger and Logger Dependencies in pom.xml
    2. Added Global Exception Handler to handle Exceptions in OrderService
    3. Used Swagger and Logger for endpoints in OrderController
    
    * Adjusted lint checks
    
    * added swagger and logger in the controller
    
    * fixed linter issues
    
    * Added Global Exception Handler and Endpoint to Check out an order
    
    * Create docker-compose.yml
    
    * Add DockerFile
    
    * Removed Checkout endpoint and Adjusted OrderStatus enum
    
    * added order address
    
    * fixed linter issues
    
    * fixed errors
    
    * Chnage long -> UUID in OrderService
    
    * long -> UUID in OrderController
    
    * Add stock reservation and order placed messaging via RabbitMQ
    
    - Added models for StockReservationRequest and StockReservationResponse
    - Added OrderPlaced event model for communicating with warehouse and ERP
    - Implemented RabbitMQ producer and consumer for messaging
    - Added RabbitMQ configuration and dependency setup
    
    * modified order consumer and order producer
    
    * removed unused classes
    
    * fixed linter issues
    
    * added track order
    
    * modified get location url
    
    * fixed linter issue
    
    * added confirmation type, fixed linter issue
    
    * added order long - lat
    
    * fixed linter issue
    
    * fixed run errors
    
    ---------
    
    Co-authored-by: Omar Aly <omarosama2114@gmail.com>

--- src/main/java/com/podzilla/order/exception/GlobalExceptionHandler.java
+++ src/main/java/com/podzilla/order/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package com.podzilla.order.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,22 +9,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ErrorResponse> handleAccessDeniedException(
-            final AccessDeniedException exception) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(exception.getMessage(),
-                        HttpStatus.FORBIDDEN));
-    }
-
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handleAuthenticationException(
-            final AuthenticationException exception) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponse(exception.getMessage(),
-                        HttpStatus.UNAUTHORIZED));
-    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(

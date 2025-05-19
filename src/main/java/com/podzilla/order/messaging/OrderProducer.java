21a8b9246411cd2a8f80cc48ab2fd2f8e798e52b
Nour Eldien Ayman <49641430+NourAlPha@users.noreply.github.com>
2025-05-19 18:02:54 +0300

    Fix order (#9)
    * Change Order relationship from ManyToOne to OneToOne in Address model
    
    * solved issues
    
    * Fix product ID assignment in CreatedOrderStrategy
    
    * Update podzilla-utils-lib version to v1.1.13
    
    ---------
    
    Co-authored-by: MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>

--- src/main/java/com/podzilla/order/messaging/OrderProducer.java
+++ src/main/java/com/podzilla/order/messaging/OrderProducer.java
@@ -40,4 +40,6 @@ public class OrderProducer {
                 orderCancelledEvent
         );
     }
+
+
 }
--- src/main/java/com/podzilla/order/model/Address.java
+++ src/main/java/com/podzilla/order/model/Address.java
@@ -1,13 +1,7 @@
 package com.podzilla.order.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,7 +25,7 @@ public class Address {
     private String country;
     private String postalCode;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 }
